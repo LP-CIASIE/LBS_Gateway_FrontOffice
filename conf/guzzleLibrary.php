f2063f7029207ea3e952d33c965587714f4b81fc
Cyprien COTINAUT <114157370+lecyp12@users.noreply.github.com>
2023-03-26 19:17:24 +0200

    Catalog to gateway (#2)
    * création branche
    
    * parametrage settings
    
    * ajout routes sandwihes
    
    * ajout route sandwiche by id
    
    * Update settings.php
    
    ---------
    
    Co-authored-by: Bradley BARBIER <bradley.barbier@outlook.fr>

--- conf/guzzleLibrary.php
+++ conf/guzzleLibrary.php
@@ -7,6 +7,13 @@ return [
     ]);
   },
 
+  'client.catalog.service' => function (\Psr\Container\ContainerInterface $c) {
+    return new \GuzzleHttp\Client([
+      'base_uri' => $c->get('catalog.service.uri'),
+      'timeout' => $c->get('timeout.request.HTTP'),
+    ]);
+  },
+
   'client.auth.service' => function (\Psr\Container\ContainerInterface $c) {
     return new \GuzzleHttp\Client([
       'base_uri' => $c->get('auth.service.uri'),
